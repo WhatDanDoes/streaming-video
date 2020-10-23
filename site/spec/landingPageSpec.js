2e0146d24b34ec2446433509842068558a10655f
Daniel Bidulock <daniel@capitolhill.ca>
2020-10-23 15:15:21 +0000

    Hello, landingPageSpec

---
+++ site/spec/landingPageSpec.js
@@ -0,0 +1,24 @@
+const Browser = require('zombie');
+const PORT = process.env.NODE_ENV === 'production' ? 3000 : 3001;
+Browser.localhost('example.com', PORT);
+const app = require('../app');
+
+describe('landing page', () => {
+  const browser = new Browser();
+
+//  beforeEach(done => {
+//    done();
+//  });
+//
+//  afterEach(() => {
+//  });
+
+  it('displays the page title set in .env', done => {
+    browser.visit('/', (err) => {
+      if (err) return done.fail(err);
+      browser.assert.success();
+      browser.assert.text('#page h1 a', process.env.TITLE);
+      done();
+    });
+  });
+});
